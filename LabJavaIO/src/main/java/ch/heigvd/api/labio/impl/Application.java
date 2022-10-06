4372093d126d5ef37de6e2bd8201f85b7d0f2c50
Killassa <gregory.reymermet@gmail.com>
2022-10-06 11:07:37 +0200

    Implement application stream writing and store quote

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/Application.java
@@ -2,9 +2,11 @@ package ch.heigvd.api.labio.impl;
 
 import ch.heigvd.api.labio.quotes.Quote;
 import ch.heigvd.api.labio.quotes.QuoteClient;
+import org.apache.commons.io.Charsets;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.logging.Level;
@@ -78,6 +80,7 @@ public class Application {
        *  Add the missing line which stores the content of the quote in a file with
        *  the name "quote-i.utf8" where 'i' is the number of the file.
        */
+      storeQuote(quote, "quote-" + i + ".utf8");
 
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
@@ -134,7 +137,9 @@ public class Application {
      *   using an output stream.
      *   Write the file with encoding UTF-8.
      */
-
+    FileOutputStream fos = new FileOutputStream(file);
+    fos.write(quote.getQuote().getBytes(Charsets.UTF_8));
+    fos.close();
   }
   
   public void processQuoteFiles() throws IOException {

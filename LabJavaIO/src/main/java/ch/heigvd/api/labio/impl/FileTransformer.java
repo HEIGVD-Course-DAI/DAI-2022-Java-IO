a88534192832cf562957c0fb29690c78245bbda6
Jarod <jarod.streckeisen@heig-vd.ch>
2022-10-10 09:46:18 +0200

    Implement the transformers

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileTransformer.java
@@ -1,6 +1,10 @@
 package ch.heigvd.api.labio.impl;
 
-import java.io.File;
+import ch.heigvd.api.labio.impl.transformers.LineNumberingCharTransformer;
+import ch.heigvd.api.labio.impl.transformers.UpperCaseCharTransformer;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,6 +19,22 @@ public class FileTransformer {
   private static final Logger LOG = Logger.getLogger(FileTransformer.class.getName());
 
   public void transform(File inputFile) {
+    try{
+      InputStreamReader input = new FileReader(inputFile, StandardCharsets.UTF_8);
+      OutputStreamWriter output = new OutputStreamWriter(new FileOutputStream(inputFile + ".out"), StandardCharsets.UTF_8);
+      int i=0;
+      UpperCaseCharTransformer ucTransform = new UpperCaseCharTransformer();
+      LineNumberingCharTransformer lnTransform = new LineNumberingCharTransformer();
+      while((i=input.read())!=-1){
+        String firstTransform = ucTransform.transform(((char)i) + "");
+        String secondTransform = lnTransform.transform(firstTransform);
+        output.write(secondTransform);
+      }
+      output.close();
+
+    }catch (Exception e){
+      e.getStackTrace();
+    }
     /*
      * This method opens the given inputFile and copies the
      * content to an output file.

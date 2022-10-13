f447852526d4a7df5a0c4c303548c2a0a051dc30
Laetitia <laetitia.guidetti@hispeed.ch>
2022-10-13 11:28:16 +0200

    complete application

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/Application.java
@@ -5,6 +5,7 @@ import ch.heigvd.api.labio.quotes.QuoteClient;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.logging.Level;
@@ -78,7 +79,7 @@ public class Application {
        *  Add the missing line which stores the content of the quote in a file with
        *  the name "quote-i.utf8" where 'i' is the number of the file.
        */
-
+      storeQuote(quote,"quote-" + i + ".utf8");
 
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
@@ -131,11 +132,11 @@ public class Application {
 
     /* Now write the quote into the file using Output streams.
      * The content of the file is in quote.getQuote().
-     * TODO: There is something missing here: you have to implement writing the file
-     *   using an output stream.
-     *   Write the file with encoding UTF-8.
      */
-
+    FileWriter fileWriter= new FileWriter(file);
+    fileWriter.write(quote.getQuote());
+    fileWriter.flush();
+    fileWriter.close();
   }
   
   public void processQuoteFiles() throws IOException {

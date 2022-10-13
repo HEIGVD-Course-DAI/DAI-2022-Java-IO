de0b1102dc0dc89c18a818eb21a006e5d6b44f89
Léo <leo.zmoos@heig-vd.ch>
2022-10-13 11:32:19 +0200

    LineNumberingTransformer implemented

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/LineNumberingCharTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/LineNumberingCharTransformer.java
@@ -4,56 +4,42 @@ import java.util.logging.Logger;
 
 /**
  * This class applies a transformation to the input character (a string with a single character):
- *   1. Convert all line endings to Unix-style line endings, i.e. only '\n'.
- *   2. Add a line number at the beginning of each line.
+ * 1. Convert all line endings to Unix-style line endings, i.e. only '\n'.
+ * 2. Add a line number at the beginning of each line.
  * Example:
- *   Using this character transformer, the following file :
- *      This is the first line.\r\n
- *      This is the second line.
- *   will be transformed to:
- *      1. This is the first line.\n
- *      2. This is the second line.
+ * Using this character transformer, the following file :
+ * This is the first line.\r\n
+ * This is the second line.
+ * will be transformed to:
+ * 1. This is the first line.\n
+ * 2. This is the second line.
  *
  * @author Olivier Liechti, Juergen Ehrensberger
  */
 public class LineNumberingCharTransformer {
-  private static final Logger LOG = Logger.getLogger(LineNumberingCharTransformer.class.getName());
+    private static final Logger LOG = Logger.getLogger(LineNumberingCharTransformer.class.getName());
 
-  // Used as flag
-  boolean firstChar = true;
+    // Used as flag
+    boolean firstChar = true;
+    int lineNumber = 1;
 
-  int lineNumber = 0;
-  public String transform(String c) {
-    /* TODO: implement the transformation here. */
+    public String transform(String c) {
+        if (firstChar) {
+            firstChar = false;
+            if (c.equals("\n")) return lineNumber++ + ". " + c + lineNumber + ". ";
+            return lineNumber + ". " + c;
+        } else {
+            switch (c) {
+                case "\r":
+                    return "";
 
-    String lines[] = c.split("\\r");
-    String temp;
-    String temp2 = c;
+                case "\n":
+                    return c + (++lineNumber) + ". ";
 
-
-    c = "";
-
-    for (int i = 0; i < lines.length; ++i)
-    {
-      if (temp2.equals("\n")){
-        firstChar = true;
-        lineNumber++;
-      }
-
-      if(firstChar){
-        temp = (i+1) + ". ";
-        temp += lines[i];
-        temp.replace('\r','\n');
-        c += temp;
-        firstChar = false;
-      }
-      else return temp2;
+                default:
+                    return c;
+            }
+        }
     }
-
-    return c;
-
-
-
-    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
 }
+//throw new UnsupportedOperationException("The student has not implemented this method yet.");
